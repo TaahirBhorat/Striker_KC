de05e00421c5d957cf8efbead96ed3971e29cecf
TaahirBhorat <taahir.bhorat3@gmail.com>
2024-03-12 12:46:43 +0200

    new ending

---
+++ pages/10_Fin.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("Final Thoughts...")
+
+st.write("We think overall the local PSL market especially when considering only South Africans forwards is sparse")
+st.write("Hence Iqraam Rayners presents a short term quality solution, one that we are confident gives room and time in the near future to bring in a truly quality international forward.")
+st.write("Ashley Cupido on the other hand presents a cheaper yet less proven option as well, with a consistent goal scoring track so far this season, as well as an impressive NFD campaign.")
+st.write("Although thorough, this analysis is limited in breadth of players chosen as well as depth of analysis. Further work could be done to scout foreign players(across tens of leagues) as well as match team styles and more granular playing playstyles")
