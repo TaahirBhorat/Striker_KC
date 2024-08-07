fff8d09b6ed2e7219e8ff493615fa1e279cc96ea
= <=>
2024-08-07 22:31:00 +0200

    field shit 2

--- pages/9_Team Compare.py
+++ pages/9_Team Compare.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.image('data/Tilt_plot.png', caption='Per Team Field Tilt - PSL 2023/2024')
 
-st.image('data/num_entries.png', caption='Per Team Box Entries - PSL 2023/2024')
+st.image('data/halfkak.png', caption='Per Team Box Entries - PSL 2023/2024')
